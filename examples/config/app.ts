d5511fdea854af19739fcfc5400c9f211cd9a093
Vunbo MacMini <vunbo@outlook.com>
2022-04-23 15:30:06 +0800

    feat(instance): axios.create实例创建

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,9 +1,10 @@
 import qs from 'qs'
+import type { AxiosTransformer } from '../../src/index'
 import axios from '../../src/index'
 
 axios.defaults.headers.common.test2 = 123
 
-axios({
+/* axios({
   url: '/config/post',
   method: 'post',
   data: qs.stringify({
@@ -14,12 +15,11 @@ axios({
   },
 }).then((res) => {
   console.log(res.data)
-})
-/*
-axios({
-  transformRequest: [(function(data) {
+}) */
+/* axios({
+  transformRequest: [function(data) {
     return qs.stringify(data)
-  }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+  }, ...(axios.defaults.transformRequest as AxiosTransformer[])],
   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
     if (typeof data === 'object') {
       data.b = 2
@@ -29,30 +29,30 @@ axios({
   url: '/config/post',
   method: 'post',
   data: {
-    a: 1
-  }
+    a: 1,
+  },
 }).then((res) => {
   console.log(res.data)
-})
+}) */
 
 const instance = axios.create({
-  transformRequest: [(function(data) {
+  transformRequest: [function(data) {
     return qs.stringify(data)
-  }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+  }, ...(axios.defaults.transformRequest as AxiosTransformer[])],
   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
     if (typeof data === 'object') {
       data.b = 2
     }
     return data
-  }]
+  }],
 })
 
 instance({
   url: '/config/post',
   method: 'post',
   data: {
-    a: 1
-  }
+    a: 1,
+  },
 }).then((res) => {
   console.log(res.data)
-}) */
+})
--- src/axios.ts
+++ src/axios.ts
@@ -1,15 +1,22 @@
-import type { AxiosInstance, AxiosRequestConfig } from './types/index'
+import type { AxiosRequestConfig, AxiosStatic } from './types/index'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './default'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
 
   const instance = Axios.prototype.request.bind(context)
 
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
-export default createInstance(defaults)
+
+const axios = createInstance(defaults)
+axios.create = function(config: AxiosRequestConfig) {
+  return createInstance(mergeConfig(defaults, config))
+}
+
+export default axios
